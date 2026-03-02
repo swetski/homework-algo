960ef725d035579c429adde7fe9ef564fe0b8712
swetski <vetlanalex@mail.ru>
2026-03-02 22:48:10 +0300

    Update person_model.py

--- zadanie_9_mvvm/models/person_model.py
+++ zadanie_9_mvvm/models/person_model.py
@@ -1,1 +1,23 @@
+from core.observable import Observable
 
+
+class PersonModel(Observable):
+    """Модель хранит данные и операции
+    """
+
+    def __init__(self, name=""):
+        super().__init__()
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    def set_name(self, value):
+        if value == self._name:
+            return
+        self._name = value
+        self.notify("name")
+
+    def clear(self):
+        self.set_name("")
