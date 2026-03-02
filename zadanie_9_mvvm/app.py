622d21571e12e8126bcb1ec2eaea5d13b8f8efd9
swetski <vetlanalex@mail.ru>
2026-03-02 22:57:52 +0300

    Update app.py

--- zadanie_9_mvvm/app.py
+++ zadanie_9_mvvm/app.py
@@ -1,1 +1,20 @@
+import tkinter as tk
+from models.person_model import PersonModel
+from viewmodels.person_viewmodel import PersonViewModel
+from views.main_view import create_view
 
+
+def main():
+    root = tk.Tk()
+    root.title("Mini MVVM Framework")
+
+    model = PersonModel("Вася")
+    vm = PersonViewModel(model)
+
+    create_view(root, vm)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
