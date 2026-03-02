bd2a43150d8518ce92a0eecdd0d7f8db08907308
swetski <vetlanalex@mail.ru>
2026-03-02 22:51:06 +0300

    Update person_viewmodel.py

--- zadanie_9_mvvm/viewmodels/person_viewmodel.py
+++ zadanie_9_mvvm/viewmodels/person_viewmodel.py
@@ -1,1 +1,26 @@
+from core.viewmodel_base import ViewModelBase
 
+
+class PersonViewModel(ViewModelBase):
+    """ViewModel управляет моделью и уведомляет View
+    """
+
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+        self.model.subscribe(self._on_model_changed)
+
+    def _on_model_changed(self, field_name):
+        if field_name == "name":
+            self.notify_vm("name")
+
+    @property
+    def name(self):
+        return self.model.name
+
+    @name.setter
+    def name(self, value):
+        self.model.set_name(value)
+
+    def clear_command(self):
+        self.model.clear()
