e5f0c675f21413accb796286e4aaee60b98611d4
swetski <vetlanalex@mail.ru>
2026-03-02 22:46:35 +0300

    Update viewmodel_base.py

--- zadanie_9_mvvm/core/viewmodel_base.py
+++ zadanie_9_mvvm/core/viewmodel_base.py
@@ -1,1 +1,14 @@
+class ViewModelBase:
+    """
+    Базовый класс ViewModel - View подписываются на свойства VM
+    """
 
+    def __init__(self):
+        self._bindings = {}
+
+    def bind(self, prop, callback):
+        self._bindings.setdefault(prop, []).append(callback)
+
+    def notify_vm(self, prop):
+        for cb in self._bindings.get(prop, []):
+            cb()
