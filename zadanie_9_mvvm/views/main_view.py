57582e48c9b17b1a9213d9d639380a88b2ae2f4d
swetski <vetlanalex@mail.ru>
2026-03-02 22:56:08 +0300

    Update main_view.py

--- zadanie_9_mvvm/views/main_view.py
+++ zadanie_9_mvvm/views/main_view.py
@@ -1,1 +1,12 @@
+import tkinter as tk
+from views.widgets import BoundEntry, BoundButton
 
+
+def create_view(root, vm):
+    tk.Label(root, text="Мини MVVM Demo", font=("Arial", 14)).pack(pady=8)
+
+    BoundEntry(root, vm, "name", "Поле 1:")
+    BoundEntry(root, vm, "name", "Поле 2:")
+    BoundEntry(root, vm, "name", "Поле 3:")
+
+    BoundButton(root, "Очистить", vm.clear_command)
