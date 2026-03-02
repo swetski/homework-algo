95c4e66cf5335f4db57fdd9c39dbaa647c4085a1
swetski <vetlanalex@mail.ru>
2026-03-02 22:41:21 +0300

    Update observable.py

--- zadanie_9_mvvm/core/observable.py
+++ zadanie_9_mvvm/core/observable.py
@@ -1,1 +1,14 @@
+class Observable:
+    """
+    Базовый класс для модели - позволяет подписываться на изменения
+    """
 
+    def __init__(self):
+        self._listeners = []
+
+    def subscribe(self, listener):
+        self._listeners.append(listener)
+
+    def notify(self, field_name):
+        for listener in list(self._listeners):
+            listener(field_name)
